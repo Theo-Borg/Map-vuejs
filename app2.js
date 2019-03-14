7945ffc50da30c87219f5f9da567cd91460f6772
Theo-Borg <theo.borg2@etu.univ-lorraine.fr>
2019-03-14 17:29:06 +0100

    first commit

---
+++ app2.js
@@ -0,0 +1,54 @@
+
+        // //var p = new Proxy('tcp://www-cache.iutnc.univ-lorraine.fr:3128/') 
+        
+        // //Gestion de la map
+        // var map = L.map('map').setView([51.505, -0.09], 13);
+
+        // L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
+        // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        // maxZoom: 18,
+        // //id: 'mapbox.streets',
+        // //accessToken: 'your.mapbox.access.token'
+        // }).addTo(map);
+    
+        // var marker = L.marker([51.5, -0.09]).addTo(map);
+
+        // var circle = L.circle([51.508, -0.11], {
+        // color: 'red',
+        // fillColor: '#f03',
+        // fillOpacity: 0.5,
+        // radius: 500
+        // }).addTo(map);
+
+        // var polygon = L.polygon([
+        // [51.509, -0.08],
+        // [51.503, -0.06],
+        // [51.51, -0.047]
+        // ]).addTo(map);
+
+        // marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+        // circle.bindPopup("I am a circle.");
+        // polygon.bindPopup("I am a polygon.");
+
+        // var popup = L.popup()
+        // .setLatLng([51.5, -0.09])
+        // .setContent("I am a standalone popup.")
+        // .openOn(map);
+
+        // function onMapClick(e) {
+        //     alert("You clicked the map at " + e.latlng);
+        // }
+
+        // map.on('click', onMapClick);
+
+        // //var popup = L.popup();
+
+        // function onMapClick(e) {
+        //     popup
+        //     .setLatLng(e.latlng)
+        //     .setContent("You clicked the map at " + e.latlng.toString())
+        //     .openOn(map);
+        // }
+
+        // map.on('click', onMapClick)
+
